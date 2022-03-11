5e0794efad83e800a4fc149d2a93d183e1532d51
Lucas Soares <soares0lucas8@gmail.com>
2022-03-11 00:13:18 -0300

    Find movies

---
+++ backend/src/main/java/com/expert/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.expert.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.expert.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+	
+}
